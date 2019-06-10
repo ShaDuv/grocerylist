91fe1f8e2d5f8ef57cec448393cfa4a1a55ad4ce
Josh Mabry and Shawna Duvall <shawna@>
2019-06-10 16:33:51 -0700

    add clear button

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,17 @@ $(document).ready(function () {
     var item = ($('.add').val()).toUpperCase();
     $('.add').val('');
     lists.push(item);
-    // lists.sort();
     console.log(lists);
-  });
-
-  // var abcList = lists.map(function (lists) {
-  //   return lists.sort();
-  // });
-
-  $('#submit').click(function () {
+  }); $('#submit').click(function () {
     lists.forEach(function (list) {
-      lists.sort();
+        lists.sort();
         console.log(list);
         $('ul#outputlist').append('<li>' + list + '</li>');
       });
+      
+    $('#clear').click(function () {
+          lists = [];
+          $('ul#outputlist').empty();
+        });
   });
 });
