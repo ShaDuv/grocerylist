6176fdb0d12e65a271ddac874b575994dfe24dd0
Josh Mabry and Shawna Duvall <josh@>
2019-06-10 16:04:44 -0700

    adds sort to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function () {
   var lists = [];
+
   $('#addbtn').click(function () {
     var item = ($('.add').val()).toUpperCase();
     $('.add').val('');
     lists.push(item);
+    // lists.sort();
     console.log(lists);
   });
+
+  // var abcList = lists.map(function (lists) {
+  //   return lists.sort();
+  // });
+
   $('#submit').click(function () {
     lists.forEach(function (list) {
+      lists.sort();
         console.log(list);
-        $('ul#outputlist').prepend('<li>' + list + '</li>');
+        $('ul#outputlist').append('<li>' + list + '</li>');
       });
   });
 });
